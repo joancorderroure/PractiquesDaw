a272fe1b16203d75d0a926ee40f3d0859c748bd9
Joan Corderroure <jcorderroure@wearejust.digital>
2020-10-27 11:59:51 +0100

    iokese

--- limitador.js
+++ limitador.js
@@ -1,8 +1,15 @@
-function limit(maximoCaracteres) {
-    var elemento = document.getElementById("text");
-    console.log(elemento);
+function limitador(){
 
-    if(elemento.value.length >= maximoCaracteres ) {
+    limit(10);
+
+}
+
+
+function limit(maximoCaracteres) {
+    var elemento = document.getElementById("text").value;
+    console.log(elemento);
+    
+    if(elemento.length >= maximoCaracteres ) {
       return false;
     }
     else {
